ca0ef4f6c26627ed858be7434d71f4a9f24125be
Terrell V (MirPresT) <terrell.vest@gmail.com>
2016-01-22 14:56:39 -0800

    committing so I can work on project on other computer

--- _src/portfolio/projects-md/web/tic-tac-toe.md
+++ _src/portfolio/projects-md/web/tic-tac-toe.md
@@ -1,1 +1,39 @@
-In Progress
+### Project Description
+This application allows the user to play “Tic Tac Toe” against the computer. The computer's skill level can be set to various difficulties.
+Choose easy and the computer will play in random locations. Pick regular and the computer will only react to certain conditions. Finally, if you set the difficulty to impossible, the computer will make the most ideal move every single time. Each game will result in a tie or a loss
+
+### Used In App
+React, Flux, Grunt, Browserify, Sass, and GithubPages.
+
+### Challenges
+This was the first application I built using both react and flux. Previously I had only used Angular while managing state in an unorganized manor.
+
+Writing the logic for the computer in both the regular and impossible game modes proved to be more challenging than expected. The application had to be setup so that each time the user made a move, the computer evaluated the state of the board and made the best move.
+
+After writing the entire app, It was brought to my attention that algorithms exist for implementing such logic. However, the code and ideas used in this application came simply from my analysis of many tic-tac-toe games.
+
+### Design
+As you will find with most of my projects, I enjoy taking the time to design the visual representation of an app before I write any meaningful code. This particular app was designed in Adobe Illustrator.
+
+
+<img id="" src="../../assets/images/tic-tac-toe/mobile-mockups.jpg" alt="">
+
+This app uses many concepts outlined in Google's material design documentation. With that said, only a miniscule amount of their pre written styles were used. For the purpose of learning I strive to personally code features or specific styling that I've yet to use myself.
+
+Below are images of the various screens of the app I created during the design process.
+
+<img id="" src="../../assets/images/tic-tac-toe/mobile-mockups.jpg" alt="">
+<img id="" src="../../assets/images/tic-tac-toe/Desktop-mockups.jpg" alt="">
+
+### Lets See It
+
+<div class="btn-container">
+  <a href="http://mirprest.github.io/tic-tac-toe/" target="_blank">
+    <div class="toApp btn">View App</div>
+  </a>
+</div>
+<div class="btn-container">
+  <a href="https://github.com/MirPresT/tic-tac-toe" target="_blank">
+    <div class="toGithub btn">&lt;View Code&gt;</div>
+  </a>
+</div>
--- gulpfile.js
+++ gulpfile.js
@@ -14,26 +14,29 @@ const buildPortfolioIndex = require('./site-build-utills/renderPortfolioIndex.js
 const buildPortfolio = require('./site-build-utills/renderPortfolio.js');
 
 /* custom render functions */
-function buildArrays(){
+function buildArrays() {
   posts.build(posts.startData);
   projects.build(projects.startData);
 }
-function buildAbout_P(){
+
+function buildAbout_P() {
   return new Promise(buildAbout)
 }
-function buildPort_P(){
+
+function buildPort_P() {
   // compile portfolio index page
   const iP = new Promise(buildPortfolioIndex);
   // Compile the portfolio posts into their own folders giving each their own index file
-  const bP = new Promise(buildPortfolio.bind(null,'business', projects.business(), projects));
-  const wP = new Promise(buildPortfolio.bind(null,'web', projects.web(), projects));
-  return Promise.all( [iP,bP,wP] );
-}
-function buildBlog_P(){
-  return new Promise(buildBlog.bind(null,posts));
+  const bP = new Promise(buildPortfolio.bind(null, 'business', projects.business(), projects));
+  const wP = new Promise(buildPortfolio.bind(null, 'web', projects.web(), projects));
+  return Promise.all([iP, bP, wP]);
 }
 
-gulp.task('render',function(){
+function buildBlog_P() {
+  return new Promise(buildBlog.bind(null, posts));
+}
+
+gulp.task('render', function() {
   // Promises excecute at the same time not in any order
   buildArrays(); // build arrays must come first or arrays won't be formatted correctly
   return Promise.all([buildPort_P(), buildBlog_P(), buildAbout_P()]);
@@ -42,7 +45,7 @@ gulp.task('render',function(){
 /* normal gulp stuff below */
 
 /* Browser Sync Task*/
-gulp.task('browser-sync',['watch'], function(){
+gulp.task('browser-sync', ['watch'], function() {
   browserSync.init({
     server: {
       baseDir: './_dist'
@@ -54,77 +57,83 @@ gulp.task('browser-sync',['watch'], function(){
 
 });
 /*Gulp Task for the initial render sequence*/
-gulp.task('sass', ['jade'], function(){
-  gulp.src('_src/assets/styles/main.scss', {base: '_src'})
+gulp.task('sass', ['jade'], function() {
+  gulp.src('_src/assets/styles/main.scss', {
+      base: '_src'
+    })
     .pipe(sass())
     .pipe(gulp.dest('_dist'))
     .pipe(browserSync.stream());
 });
 /*Gulp Task for updating without dependencies*/
-gulp.task('sass-update', function(){
-  gulp.src('_src/assets/styles/main.scss', {base: '_src'})
+gulp.task('sass-update', function() {
+  gulp.src('_src/assets/styles/main.scss', {
+      base: '_src'
+    })
     .pipe(sass())
     .pipe(gulp.dest('_dist'))
     .pipe(browserSync.stream());
 });
-gulp.task('jade',['clientJs'], function(){
+gulp.task('jade', ['clientJs'], function() {
   gulp.src([
-    '_src/**/*.jade',
-    '!_src/{assets,assets/**}',
-    '!_src/{blog,blog/**}',
-    '!_src/{portfolio,portfolio/**}',
-    '!_src/index.jade'
-  ])
+      '_src/**/*.jade',
+      '!_src/{assets,assets/**}',
+      '!_src/{blog,blog/**}',
+      '!_src/{portfolio,portfolio/**}',
+      '!_src/index.jade'
+    ])
     .pipe(jade())
     .pipe(gulp.dest('_dist'));
 });
-gulp.task('jade-update', function(){
+gulp.task('jade-update', function() {
   gulp.src([
-    '_src/**/*.jade',
-    '!_src/{assets,assets/**}',
-    '!_src/{blog,blog/**}',
-    '!_src/index.jade'
-  ])
+      '_src/**/*.jade',
+      '!_src/{assets,assets/**}',
+      '!_src/{blog,blog/**}',
+      '!_src/index.jade'
+    ])
     .pipe(jade())
     .pipe(gulp.dest('_dist'));
 });
-gulp.task('clientJs',['render'], function(){
-  gulp.src('./_src/assets/scripts/client.js', {base: '_src'})
+gulp.task('clientJs', ['render'], function() {
+  gulp.src('./_src/assets/scripts/client.js', {
+      base: '_src'
+    })
     .pipe(gulp.dest('_dist'));
 });
-gulp.task('clientJs-update', function(){
-  gulp.src('./_src/assets/scripts/client.js', {base: '_src'})
+gulp.task('clientJs-update', function() {
+  gulp.src('./_src/assets/scripts/client.js', {
+      base: '_src'
+    })
     .pipe(gulp.dest('_dist'));
   buildPort_P().then(browserSync.reload);
 });
-gulp.task('watch',['sass'], function(){
-  gulp.watch('./_src/assets/styles/**/*.scss',['sass-update']);
-  gulp.watch('./_src/assets/scripts/client.js',['clientJs-update']);
+gulp.task('watch', ['sass'], function() {
+  gulp.watch('./_src/assets/styles/**/*.scss', ['sass-update']);
+  gulp.watch('./_src/assets/scripts/client.js', ['clientJs-update']);
   gulp.watch('./_src/assets/markup/nav.jade', ['jade-update']);
+  // watch about page
   gulp.watch([
     './_src/_about/*.md',
     './_src/*.jade'
-  ], function(){
+  ], function() {
     buildAbout_P().then(browserSync.reload);
   });
+  // watch blog stuff
   gulp.watch([
     './_src/blog/md-posts/*.md',
     './_src/blog/*.jade',
     './_src/assets/markup/post-template.jade'
-  ], function(){
-    buildBlog_P().then(function(){
+  ], function() {
+    buildBlog_P().then(function() {
       browserSync.reload();
     });
   });
-  gulp.watch([
-    './_src/portfolio/projects-md/**/*.md',
-    './_src/portfolio/*.jade',
-    './_src/assets/markup/portfolio-template.jade'
-  ], function(){
-    buildPort_P().then(function(){
-      buildApp_P().then(browserSync.reload());
-    })
+  // watch portfolio stuff
+  gulp.watch('./_src/portfolio/projects-md/web/*.md',function(){
+    console.log('file changed');
+    buildPort_P().then(browserSync.reload)
   })
 });
 
-gulp.task('default', ['render','clientJs','jade','sass','watch','browser-sync']);
+gulp.task('default', ['render', 'clientJs', 'jade', 'sass', 'watch', 'browser-sync']);
